ce7512c565b221a5d20daebb04e288bc97d5bb00
Misho <work.1999line@gmail.com>
2026-07-11 18:08:09 +0400

    profile5

--- src/app/environment/Environment.production.ts
+++ src/app/environment/Environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000' // შენი პროდ API
+  apiUrl: 'https://ggzavnabeck.onrender.com' // შენი პროდ API
 };
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000' // შენი ბექენდის URL
+  apiUrl: 'https://ggzavnabeck.onrender.com' // შენი ბექენდის URL
 };
  
