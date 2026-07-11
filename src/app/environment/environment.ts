e6a93a93522770f256c54353f669ae9823497fda
Misho <work.1999line@gmail.com>
2026-07-12 00:17:48 +0400

    cors3

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://ggzavnabeck.onrender.com' // შენი ბექენდის URL
+  apiUrl: 'http://localhost:10000' // შენი ბექენდის URL
 };
  
