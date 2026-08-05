189f2d9ea12269ecc6438a55ee9af349f0b0df16
Giorgi <giorgi@gmail.com>
2026-08-05 02:32:28 -0700

    navbarprofile

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class FooterComponent {
   constructor(private router: Router) {}
 
-  /**
-   * სხვადასხვა გვერდებზე გადასვლა და გვერდის ავტომატურად თავში ატანა
-   */
-  navigateTo(path: string): void {
-    this.router.navigate([path]).then(() => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-  }
 }
